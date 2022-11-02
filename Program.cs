1cc7e446d237fd760dbe64643ce0f7dae3988868
e.polovinkin <polkill@list.ru>
2022-11-02 19:27:33 +0300

    sem 1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+// семинар 1
+
+// // 1. Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+// Console.Write("Введите число: ");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// // int sqrt = number * number;
+// // number *= number;
+// int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+
+// Console.WriteLine("число в квадрате: " + sqrt1);
+
+// 2. Напишите программу которая на вход принимает трехзначное число и на выходе показывает последнуюю цифру этого числа
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99 && num < 999)
+{
+    Console.Write("Последняя цифра: " + Math.Abs(num) % 10);
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
